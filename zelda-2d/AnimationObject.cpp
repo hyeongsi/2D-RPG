3455f96ae9bcfbc6f5161829b0b3293174d0bc0b
parkSihyeong46 <71202869+parkSihyeong46@users.noreply.github.com>
2021-01-12 21:21:56 +0900

    애니메이션 클래스 추가

---
+++ zelda-2d/AnimationObject.cpp
@@ -0,0 +1,34 @@
+﻿#include "pch.h"
+#include "AnimationObject.h"
+
+const HBITMAP AnimationObject::GetAnimationBitmap(const int index)
+{
+	if (index > static_cast<int>(bitmapData.size()) && 0 > index)
+		return nullptr;
+
+	return bitmapData[index];
+}
+
+void AnimationObject::SetBitmap(const int index, const HBITMAP hbitmap)
+{
+	if (index > static_cast<int>(bitmapData.size()) && 0 > index)
+		return;
+
+	bitmapData[index] = hbitmap;
+}
+
+const POINT AnimationObject::GetNormalPos(const int index)
+{
+	if (index > static_cast<int>(normalPos.size()) && 0 > index)
+		return { NULL, NULL };
+
+	return normalPos[index];
+}
+
+void AnimationObject::SetNormalPos(const int index, const POINT pos)
+{
+	if (index > static_cast<int>(normalPos.size()) && 0 > index)
+		return;
+
+	normalPos[index] = pos;
+}
---
+++ zelda-2d/AnimationObject.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include <vector>
+
+using namespace std;
+
+class AnimationObject
+{
+private:
+	vector<HBITMAP> bitmapData;				
+	vector<POINT> normalPos;				
+
+public:
+	const HBITMAP GetAnimationBitmap(const int index);
+	void SetBitmap(const int index, const HBITMAP hbitmap);
+
+	const POINT GetNormalPos(const int index);
+	void SetNormalPos(const int index, const POINT pos);
+};
